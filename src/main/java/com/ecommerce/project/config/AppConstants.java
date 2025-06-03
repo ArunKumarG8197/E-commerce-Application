fd6be9b4c51a30089f64116e69189fdbd6f5783e
Arun-Kumar-G-2002 <108813225+ArunKumarG8197@users.noreply.github.com>
2025-06-03 11:06:32 +0530

    initial commit

---
+++ src/main/java/com/ecommerce/project/config/AppConstants.java
@@ -0,0 +1,9 @@
+package com.ecommerce.project.config;
+
+public class AppConstants {
+
+    public static final String DEFAULT_PAGE_NUMBER = "0";
+    public static final String DEFAULT_PAGE_SIZE = "10";
+    public static final String SORT_CATEGORIES_BY = "categoryId";
+    public static final String SORT_DIR = "asc";
+}
